7d985786e3340480618b8f1b03394be88d29b651
vanph <vanphbk@gmail.com>
2018-01-15 00:02:09 +0700

    Binding List

--- dev/BindingExample/Window1.xaml.cs
+++ dev/BindingExample/Window1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,23 +20,33 @@ namespace BindingExample
     /// </summary>
     public partial class Window1 : Window
     {
-        private readonly Person _person = new Person {Name = "Ian", Age = 34, Address = "Hanoi"};
-
+        public ObservableCollection<Person> People { get; set; }
         public Window1()
         {
             InitializeComponent();
 
-            this.DataContext = _person;
+            var persons = new List<Person>
+            {
+                new Person {Name = "Ian", Age = 34, Address = "Hanoi"},
+                new Person {Name = "Ian2", Age = 12, Address = "Saigon"},
+                new Person {Name = "Ian3", Age = 45, Address = "Soc trang"},
+                new Person {Name = "Ian4", Age = 65, Address = "Thai nguyen"},
+                new Person {Name = "Ian5", Age = 76, Address = "Nam dinh"}
+            };
+
+            People = new ObservableCollection<Person>(persons);
+            
+            DataContext = this;
         }
-        
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"{_person.Name} is {_person.Age}");
+            //MessageBox.Show($"{_person.Name} is {_person.Age}");
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            _person.Age += 1;
+            //_person.Age += 1;
         }
     }
 }
