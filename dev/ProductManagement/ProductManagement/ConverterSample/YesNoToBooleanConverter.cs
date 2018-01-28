894a4b7c0b7411559759e35e80869283fe903e5f
vanph <vanphbk@gmail.com>
2018-01-28 23:40:04 +0700

    Converter ; Binding

--- dev/ProductManagement/ProductManagement/ConverterSample/YesNoToBooleanConverter.cs
+++ dev/ProductManagement/ProductManagement/ConverterSample/YesNoToBooleanConverter.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows.Data;
 
 namespace ConverterSample
 {
-   public class YesNoToBooleanConverter: IValueConverter
+    public class YesNoToBooleanConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             switch (value.ToString().ToLower())
             {
@@ -20,16 +21,15 @@ namespace ConverterSample
             }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is bool)
             {
-                return (bool)value ? "yes" : "no";
+                return (bool)value ? "YES" : "NO";
             }
 
             return "no";
-
-            //throw new NotImplementedException();
         }
     }
+
 }
--- dev/ProductManagement/ProductManagement/ProductManagement/ViewModels/ProductListViewModel.cs
+++ dev/ProductManagement/ProductManagement/ProductManagement/ViewModels/ProductListViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using ProductManagement.Framework;
 using ProductManagement.Models;
 using ProductManagement.Repositories;
+using ProductManagement.Views;
 
 namespace ProductManagement.ViewModels
 {
@@ -22,6 +23,16 @@ namespace ProductManagement.ViewModels
             }
         }
 
+        private Product _selectedProduct;
+        public Product SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                _selectedProduct = value;
+                RaisePropertyChanged(nameof(SelectedProduct));
+            }
+        }
         public DelegateCommand AddCommand { get; private set; }
 
         public DelegateCommand EditCommand { get; private set; }
@@ -36,8 +47,8 @@ namespace ProductManagement.ViewModels
             InitializeCommands();
             Products = new ObservableCollection<Product>(_productRepository.GetProducts());
         }
-        
-        
+
+
         private void InitializeCommands()
         {
             AddCommand = new DelegateCommand(o => this.OnAddCommandExecute());
@@ -45,25 +56,33 @@ namespace ProductManagement.ViewModels
             DeleteCommand = new DelegateCommand(o => this.OnDeleteCommandExecute());
             RefreshCommand = new DelegateCommand(o => this.RefreshProducts());
         }
-        
+
         private void RefreshProducts()
         {
-            
+            MessageBox.Show(nameof(RefreshProducts));
         }
 
         private void OnAddCommandExecute()
         {
-            
+            var productDetailView = new ProductDetailView();
+            productDetailView.ShowDialog();
         }
 
         private void OnEditCommandExecute()
         {
+            if (SelectedProduct != null)
+            {
+                MessageBox.Show(SelectedProduct.Name);
+            }
         }
 
         private void OnDeleteCommandExecute()
         {
-           
+            if (SelectedProduct != null)
+            {
+                MessageBox.Show(SelectedProduct.Name);
+            }
         }
-        
+
     }
 }
