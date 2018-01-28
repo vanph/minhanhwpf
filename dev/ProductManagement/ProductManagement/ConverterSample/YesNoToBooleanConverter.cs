2d41402547dd5bccad7c6eb62b8c6c2fb5bb72f7
vanph <vanphbk@gmail.com>
2018-01-28 18:16:14 +0700

    Add converter sample

---
+++ dev/ProductManagement/ProductManagement/ConverterSample/YesNoToBooleanConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace ConverterSample
+{
+   public class YesNoToBooleanConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "yes":
+                case "oui":
+                    return true;
+                case "no":
+                case "non":
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? "yes" : "no";
+            }
+
+            return "no";
+
+            //throw new NotImplementedException();
+        }
+    }
+}
