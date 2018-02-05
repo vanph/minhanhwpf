cd0bbbfe3c8d063e54f3fe387b76a5e99f17b60b
Minh MInh <phamanhminh231@gmail.com>
2018-02-05 18:00:29 +0300

    Converter

---
+++ dev/ProductManagement/ProductManagement/ProductManagement/Converters/StringToCurrencyConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ProductManagement.Converters
+{
+    public class StringToCurrencyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            culture = new CultureInfo("en-US");
+            if (value != null)
+            {
+                double number = double.Parse(value.ToString());
+                return number.ToString("C",culture);
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
