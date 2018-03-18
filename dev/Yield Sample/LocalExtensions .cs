43b7404950f8a0ea2db53d0b5e944ad82407b525
vanph <vanphbk@gmail.com>
2018-03-18 21:34:25 +0700

    Add yield sample

---
+++ dev/Yield Sample/LocalExtensions .cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yield
+{
+    public static class LocalExtensions
+    {
+        public static IEnumerable<string>
+            ConvertCollectionToUpperCase(this IEnumerable<string> source)
+        {
+            foreach (string str in source)
+            {
+                Console.WriteLine("ToUpper: source {0}", str);
+                yield return str.ToUpper();
+            }
+        }
+    }
+
+   public static class LocalExtensionsTest
+    {
+       public static void Test()
+        {
+            string[] stringArray = { "abc", "def", "ghi" };
+
+            var q = from str in stringArray.ConvertCollectionToUpperCase()
+                select str;
+
+            foreach (string str in q)
+                Console.WriteLine("Test: str {0}", str);
+        }
+    }
+}
